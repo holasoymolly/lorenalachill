6abd7e6ab963ab5ed46477aafb85b2c0ad4f032a
Molly Yllom <hola@mollyyllom.com>
2024-11-26 19:56:17 -0400

    Actualicé fixed-plugin.tsx

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -1,22 +1,17 @@
 "use client";
-import Image from "next/image";
 import { Button } from "@material-tailwind/react";
 
 export function FixedPlugin() {
   return (
-    <a href="https://www.material-tailwind.com" target="_blank">
+    <a href="https://www.material-tailwind.com" target="_blank" rel="noopener noreferrer">
       <Button
-        color="white"
         size="sm"
-        className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}      >
-        <Image
-          width={128}
-          height={128}
-          className="w-5 h-5"
-          alt="Material Tailwind"
-          src="https://www.material-tailwind.com/favicon.png"
-        />{" "}
-        Made With Material Tailwind
+        className="!fixed bottom-4 right-4 z-50 flex gap-1 pl-6 text-white border border-white transition-transform duration-300 hover:scale-105 py-2 px-5 rounded-full bg-transparent"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+        >
+        Suscríbete a mi Newsletter
       </Button>
     </a>
   );
