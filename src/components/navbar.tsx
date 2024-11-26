4a64fc880df9bbfeadb00027a92bbc7ccf77f7db
Molly Yllom <hola@mollyyllom.com>
2024-11-26 18:40:34 -0400

    Actualicé el navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import {
   Navbar as MTNavbar,
   Collapse,
@@ -28,7 +29,11 @@ function NavItem({ children, href }: NavItemProps) {
         href={href || "#"}
         target={href ? "_blank" : "_self"}
         variant="paragraph"
-        className="flex items-center gap-2 font-medium" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}      >
+        className="flex items-center gap-2 font-medium text-white transition-colors duration-300 hover:text-[#F15927]"
+        placeholder={undefined}
+        onPointerEnterCapture={undefined}
+        onPointerLeaveCapture={undefined}
+      >
         {children}
       </Typography>
     </li>
@@ -37,83 +42,100 @@ function NavItem({ children, href }: NavItemProps) {
 
 const NAV_MENU = [
   {
-    name: "Page",
-    icon: RectangleStackIcon,
+    name: "Sobre mí",
+    href: "/about", // Ruta a la página "About"
   },
   {
-    name: "Account",
-    icon: UserCircleIcon,
+    name: "Calendario",
+    href: "/calendar", // Ruta a la página "Calendar"
   },
   {
-    name: "Docs",
-    icon: CommandLineIcon,
-    href: "https://www.material-tailwind.com/docs/react/installation",
+    name: "Preguntas Frequentes",
+    href: "/faq", // Ruta a la página "FAQ"
+  },
+  {
+    name: "Contacto",
+    href: "/contact", // Ruta a la página "Contact"
   },
 ];
 
 export function Navbar() {
   const [open, setOpen] = React.useState(false);
-  const [isScrolling, setIsScrolling] = React.useState(false);
+  const [isVisible, setIsVisible] = React.useState(true);
+  const [lastScrollY, setLastScrollY] = React.useState(0);
 
   const handleOpen = () => setOpen((cur) => !cur);
 
   React.useEffect(() => {
-    window.addEventListener(
-      "resize",
-      () => window.innerWidth >= 960 && setOpen(false)
-    );
-  }, []);
-
-  React.useEffect(() => {
-    function handleScroll() {
-      if (window.scrollY > 0) {
-        setIsScrolling(true);
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY) {
+        // Scrolling down
+        setIsVisible(false);
       } else {
-        setIsScrolling(false);
+        // Scrolling up
+        setIsVisible(true);
       }
-    }
+      setLastScrollY(currentScrollY);
+    };
 
     window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
 
   return (
     <MTNavbar
       shadow={false}
       fullWidth
       blurred={false}
-      color={isScrolling ? "white" : "transparent"}
-      className="fixed top-0 z-50 border-0" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}    >
-      <div className="container mx-auto flex items-center justify-between">
-        <Typography
-          color={isScrolling ? "blue-gray" : "white"}
-          className="text-lg font-bold" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
-          Material Tailwind
-        </Typography>
-        <ul
-          className={`ml-10 hidden items-center gap-6 lg:flex ${
-            isScrolling ? "text-gray-900" : "text-white"
-          }`}
-        >
-          {NAV_MENU.map(({ name, icon: Icon, href }) => (
+      color="transparent"
+      className={`fixed top-0 z-50 border-0 transition-transform duration-300 ${isVisible ? "translate-y-0" : "-translate-y-full"}`}
+      style={{
+        height: "6rem", // Altura fija del navbar
+        overflow: "hidden", // Asegurarnos de que nada sobresalga
+      }} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}    >
+      <div className="container mx-auto flex items-center justify-between h-full">
+        <div className="flex items-center h-full">
+        <Link href="/" className="relative group block">
+  <img
+    src="/logos/lorena-la-chill-logo-blanco.webp"
+    alt="Lorena La Chill Logo"
+    style={{ height: "3.80rem", width: "auto" }}
+    className="transition-opacity duration-300 group-hover:opacity-0"
+  />
+  <img
+    src="/logos/lorena-la-chill-logo-naranja.webp"
+    alt="Lorena La Chill Logo Hover"
+    style={{ height: "3.80rem", width: "auto" }}
+    className="absolute top-0 left-0 transition-opacity duration-300 opacity-0 group-hover:opacity-100"
+  />
+</Link>
+        </div>
+        <ul className="ml-10 hidden items-center gap-6 lg:flex text-white h-full">
+          {NAV_MENU.map(({ name, href }) => (
             <NavItem key={name} href={href}>
-              <Icon className="h-5 w-5" />
               <span>{name}</span>
             </NavItem>
           ))}
         </ul>
-        <div className="hidden items-center gap-4 lg:flex">
-          <Button color={isScrolling ? "gray" : "white"} variant="text" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
-            Log in
-          </Button>
-          <a href="https://www.material-tailwind.com/blocks" target="_blank">
-            <Button color={isScrolling ? "gray" : "white"} placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>blocks</Button>
-          </a>
-        </div>
+        <div className="hidden items-center gap-4 lg:flex h-full">
+  <a href="https://www.material-tailwind.com/blocks" target="_blank">
+  <Button
+  color="white"
+  className="text-[#F15927] transition-colors duration-300 hover:bg-[#F15927] hover:text-white"
+  placeholder={undefined}
+  onPointerEnterCapture={undefined}
+  onPointerLeaveCapture={undefined}
+>
+  Newsletter
+</Button>
+  </a>
+</div>
         <IconButton
           variant="text"
-          color={isScrolling ? "gray" : "white"}
+          color="white"
           onClick={handleOpen}
           className="ml-auto inline-block lg:hidden" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}        >
           {open ? (
@@ -123,24 +145,6 @@ export function Navbar() {
           )}
         </IconButton>
       </div>
-      <Collapse open={open}>
-        <div className="container mx-auto mt-4 rounded-lg bg-white px-6 py-5">
-          <ul className="flex flex-col gap-4 text-gray-900">
-            {NAV_MENU.map(({ name, icon: Icon, href }) => (
-              <NavItem key={name} href={href}>
-                <Icon className="h-5 w-5" />
-                {name}
-              </NavItem>
-            ))}
-          </ul>
-          <div className="mt-6 flex items-center gap-4">
-            <Button variant="text" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>Log in</Button>
-            <a href="https://www.materila-tailwind.com/blocks" target="_blank">
-              <Button color="gray" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>blocks</Button>
-            </a>
-          </div>
-        </div>
-      </Collapse>
     </MTNavbar>
   );
 }
