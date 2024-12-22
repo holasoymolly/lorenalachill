abe79ede63e1f07e0e6e11bbe8df1f2009c9c537
holasoymolly <hola@mollyyllom.com>
2024-12-22 17:05:00 -0400

    Eliminé páginas que no estoy usando en index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,8 +4,4 @@ export * from "./layout";
 export * from "./page";
 export * from "./our-stats";
 export * from "./about-event";
-export * from "./faq";
-export * from "./sponsored-by";
-export * from "./event-content";
-
 
